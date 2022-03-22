744df44629192803131a1f1fecac44278972e5a4
andresovichh <ahenderson.holberton@gmail.com>
2022-03-22 16:39:29 -0300

    1 seems to work

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as te left child of another node
+ * @parent: parent node
+ * @value: Value to put in the new node
+ * Return: binary_tree_t* or NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *childNode = NULL;
+
+childNode = malloc(sizeof(binary_tree_t));
+if (!childNode)
+return (NULL);
+
+if (!parent)
+return (NULL);
+/** create new node usin foo from 0*/
+childNode = binary_tree_node(parent, value);
+
+/** check if parent has left child */
+if (parent->left != NULL)
+{
+childNode->left = parent->left;
+parent->left->parent = childNode;
+parent->left = childNode;
+}
+else
+{
+parent->left = childNode;
+}
+
+return (childNode);
+}
