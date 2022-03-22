033aade0f7f54c87dcb989575914751b508ba17f
andresovichh <ahenderson.holberton@gmail.com>
2022-03-22 16:43:16 -0300

    2 works and is betty compliant

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a node as te right child of another node
+ * @parent: parent node
+ * @value: Value to put in the new node
+ * Return: binary_tree_t* or NULL
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+binary_tree_t *childNode = NULL;
+
+childNode = malloc(sizeof(binary_tree_t));
+if (!childNode)
+return (NULL);
+
+if (!parent)
+return (NULL);
+/** create new node usin foo from 0*/
+childNode = binary_tree_node(parent, value);
+
+/** check if parent has right child */
+if (parent->right != NULL)
+{
+childNode->right = parent->right;
+parent->right->parent = childNode;
+parent->right = childNode;
+}
+else
+{
+parent->right = childNode;
+}
+
+return (childNode);
+}
