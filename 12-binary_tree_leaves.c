7560c78a6dfe12e311d1fcd41045ea90924e71d3
andresovichh <ahenderson.holberton@gmail.com>
2022-03-23 17:26:39 -0300

    test 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - checks if node is root
+ * @tree: pointer to the root node to traverse from
+ * Return: the height of the tree
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+size_t counter = 0;
+
+if (!tree)
+return (0);
+
+if (tree->left == NULL && tree->right == NULL)
+counter += binary_tree_leaves(tree);
+return (counter);
+}
