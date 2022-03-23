072f2222c78cac6835fd6773abc1bbbd67fa5fe4
andresovichh <ahenderson.holberton@gmail.com>
2022-03-23 17:18:14 -0300

    11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - checks if node is root
+ * @tree: pointer to the root node to traverse from
+ * Return: the height of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+size_t counter = 0; 
+if (!tree)
+return (0);
+
+if (tree->left)
+counter = binary_tree_size(tree->left);
+if (tree->right)
+counter = binary_tree_size(tree->right);
+return (counter);
+}
