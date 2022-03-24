2b7365fea5486a8e54e30011989912117f0d5f33
andresovichh <ahenderson.holberton@gmail.com>
2022-03-24 09:42:48 -0300

    fixed betty 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,12 +10,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *childNode;
 
-/** 
- * childNode = malloc(sizeof(binary_tree_t));
- * if (!childNode)
- * return (NULL);
- */
-
 if (!parent)
 return (NULL);
 /** create new node usin foo from 0*/
