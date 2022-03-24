a4807e1a0ffc9329af523a32dec9abc391d686f1
andresovichh <ahenderson.holberton@gmail.com>
2022-03-24 09:48:45 -0300

    fixed betty 7 & 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if node is root
+ * binary_tree_inorder - checks if node is root
  * @tree: pointer to the root node to traverse from
  * @func: s a pointer to a function to call for each node.
  * Return: if node is NULL, return 0, if node is root, return 1
